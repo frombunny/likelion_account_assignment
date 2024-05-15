14c5e1d3e6631b932b8f260ca56c8d27caf9b1af
HyeJeong <rucy011121@gmail.com>
2024-05-16 05:42:51 +0900

    :sparkles: 회원가입 기능 - 입력 형식 지정

--- account-assignment-base-main/src/main/java/com/example/account/dto/MemberSignUpDto.java
+++ account-assignment-base-main/src/main/java/com/example/account/dto/MemberSignUpDto.java
@@ -1,7 +1,9 @@
 package com.example.account.dto;
 
 import com.example.account.domain.Member;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -23,9 +25,12 @@ public class MemberSignUpDto {
         private String password_check;
 
         @NotBlank(message = "이메일 입력란은 비어 있을 수 없습니다.")
+        @Email(message = "잘못된 이메일 형식입니다.")
         private String email;
 
+        public static final String regexp1 ="^[\\d]{11}+$"; //전화번호 형식이 010XXXXXXXX
         @NotBlank(message = "전화번호 입력란은 비어 있을 수 없습니다.")
+        @Pattern(message = "잘못된 전화번호 형식입니다.",regexp = regexp1)
         private String phone;
 
         public Member toEntity(){
